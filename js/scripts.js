6bbd53ac1638947af7c7f8c253f2907a02120c6e
Eric Wolfe and Jim Palowski <jim@>
2018-03-26 11:10:52 -0700

    allow for aoutput to web page

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
+function printitems(name, parks, rides){
+  var name = "Name"
+  var rides = "Rides"
+  var parks = "Park"
+
+}
+
+
+
+
+
+
 $(document).ready(function() {
   $("#formArray").submit(function(event) {
   event.preventDefault();
-  var userName = $("input").attr("value").val();
+  var userName = $("input[name=name]").val();
   var rides = $("#rides").val();
   var parks = $("input:radio[name=places]:checked").val();
+  var list = [userName, rides, parks]
+  var Name = list[0];
+  var Rides = list[1];
+  var Park = list[2];
+  document.getElementById('Name').innerHTML = Name;
+  document.getElementById('Park').innerHTML = Park;
+  document.getElementById('Rides').innerHTML = Rides;
 
-  console.log("USER ENTERED:" + userName + "USER ENTERED:" + rides + "USERENTERED:" + parks);
+
+  var results = printitems(list)
+  console.log("USER ENTERED:" + list[0] + "USER ENTERED:" + list[1] + "USERENTERED:" + list[2]);
+  console.log(list)
   });
 });
